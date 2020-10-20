d34d22a2ae52ed33e6918e2b3045eb61d07f3e60
franynadamas <franynadamas@outlook.com>
2020-10-20 23:27:53 +0100

    Add files via upload

---
+++ src/Ventanas/Window.java
@@ -0,0 +1,18 @@
+package Ventanas;
+
+import javax.swing.JFrame;
+public class Window extends  JFrame {
+    public static final int WIDTH=800,HEIGHT=600;
+    public Window(){
+        setTitle("OurGroove");
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Window();
+    }
+
+}
